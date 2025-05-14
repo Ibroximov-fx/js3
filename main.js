52a92f32f585d618e925f3dddd8e98eac397771d
ibroximov-fx <ibrohimovahmad91@gmail.com>
2025-05-14 09:55:20 -0700

    js3

---
+++ main.js
@@ -0,0 +1,59 @@
+// let b = 100
+// for (let i = 1; i <= b; i++) {
+//     console.log(i);
+// }
+
+
+
+// let narxi = 10000
+// let a = 0
+// let kg = "kg"
+//
+// for (let i = 1; i <= 10; i++) {
+//     console.log (i+kg)
+//     console.log(a=a+narxi);
+// }
+
+
+
+// let narxi =11000
+// let b =1000
+// let kg="kg"
+// for (let i = 1; i >= 0.1; i-=0.1) {
+//     console.log(i.toFixed(1)+kg)
+//     console.log(narxi=narxi-b)
+// }
+
+
+
+// let b=10
+// let a=0
+// for(let i=1; i<=b; i++) {
+//     console.log(i);
+//     a=a+i
+// }
+// console.log(a);
+
+
+
+// let a=10
+// let b=1
+// for(let i=1; i<=a; i++) {
+//     console.log(i);
+//     b=b*i
+// }
+// console.log(b);
+
+
+
+// for (let i=1 ; i<=100; i++){
+//     if(i % 3 ===0 && i % 5 === 0){
+//         console.log(i, "FizzBuzz");
+//     }else if(i % 3 === 0){
+//         console.log(i, "Fizz");
+//     }else if(i % 5 === 0){
+//         console.log(i, "Buzz");
+//     }else {
+//         console.log(i)
+//     }
+// }
