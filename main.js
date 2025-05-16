24d7c6ad830500ac995cec71e8825ff413e7d079
ibroximov-fx <ibrohimovahmad91@gmail.com>
2025-05-16 10:27:34 -0700

    function

--- main.js
+++ main.js
@@ -1,59 +1,77 @@
-// let b = 100
-// for (let i = 1; i <= b; i++) {
-//     console.log(i);
-// }
-
-
-
-// let narxi = 10000
-// let a = 0
-// let kg = "kg"
+// function js1 (b, c,){
 //
-// for (let i = 1; i <= 10; i++) {
-//     console.log (i+kg)
-//     console.log(a=a+narxi);
-// }
-
-
-
-// let narxi =11000
-// let b =1000
-// let kg="kg"
-// for (let i = 1; i >= 0.1; i-=0.1) {
-//     console.log(i.toFixed(1)+kg)
-//     console.log(narxi=narxi-b)
-// }
-
-
-
-// let b=10
-// let a=0
-// for(let i=1; i<=b; i++) {
-//     console.log(i);
-//     a=a+i
-// }
-// console.log(a);
-
-
-
-// let a=10
-// let b=1
-// for(let i=1; i<=a; i++) {
-//     console.log(i);
-//     b=b*i
-// }
-// console.log(b);
-
-
-
-// for (let i=1 ; i<=100; i++){
-//     if(i % 3 ===0 && i % 5 === 0){
-//         console.log(i, "FizzBuzz");
-//     }else if(i % 3 === 0){
-//         console.log(i, "Fizz");
-//     }else if(i % 5 === 0){
-//         console.log(i, "Buzz");
-//     }else {
-//         console.log(i)
+//     for (let i = 1; i <= b; i++) {
+//         console.log(`${i} ${c}`);
 //     }
 // }
+// js1(12,  "ha")
+// js1(29, "yuq")
+
+
+
+
+
+// function js2 (narxi, a, kg,){
+//
+//     for (let i = 1; i <= 10; i++) {
+//         console.log (i+kg)
+//         console.log(a=a+narxi);
+//     }
+// }
+// js2(10000, 0, "kg")
+// js2(20000, 0, "g")
+//
+//
+
+
+
+//
+//
+// function js3(narxi, b, kg){
+//     for (let i = 1; i >= 0.1; i-=0.1) {
+//         console.log(i.toFixed(1)+kg)
+//         console.log(narxi=narxi-b)
+//     }
+// }
+// js3(20000, 2000,  "kg" )
+
+
+
+// function js4(b, a){
+//
+//     for(let i=1; i<=b; i++) {
+//         console.log(i);
+//         a=a+i
+//     }
+//     console.log(a);
+// }
+// js4(20, 0, )
+
+
+//
+// function js5(a, b){
+//
+//     for(let i=1; i<=a; i++) {
+//         console.log(i);
+//         b=b*i
+//     }
+//     console.log(b);
+// }
+// js5(4, 1)
+
+
+
+// function js6(san, man, yozuv, boshi, oxiri ){
+//     for (let i=1 ; i<=100; i++){
+//         if(i % san ===0 && i % man === 0){
+//             console.log(`${i} ${yozuv}`);
+//         }else if(i % san === 0){
+//             console.log(`${i} ${boshi}`);
+//         }else if(i % man === 0){
+//             console.log(`${i} ${oxiri}`);
+//         }else {
+//             console.log(i)
+//         }
+//     }
+// }
+// js6(3, 5, "fizzbuzz", "fizz", "buzz")
